114375b4b79b27fb51ad703894aa8e2296b35fd8
bbaution <bautiramall01@gmail.com>
2026-06-08 19:42:19 -0300

    ultimos detalles

--- bar-pedidos/backend/src/db.js
+++ bar-pedidos/backend/src/db.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql2/promise");
+/* const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const pool = mysql.createPool({
@@ -6,6 +6,19 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+});
+
+module.exports = pool; */
+const mysql = require("mysql2/promise");
+
+const pool = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
   waitForConnections: true,
   connectionLimit: 10,
 });
